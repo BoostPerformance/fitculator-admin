06a0e8151523105dabbad087314cbf3744aab336
sung-eunji <danceunji@gmail.com>
2024-10-07 15:47:58 +0200

    chore: change component structure of user

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,16 +1,27 @@
-import Dashboard from './dashboard/page';
-import Header from '@/components/userPage/dashboardHeader';
+'use client';
 import Sidebar from '@/components/userPage/sidebar';
+import Stats from '@/components/graph/stats';
+import SalesChart from '@/components/graph/salesChart';
+import TrafficSourceChart from '@/components/graph/trafficSourceChart';
+import TopReferrals from '@/components/graph/topReferrals';
+import LatestProjects from '@/components/graph/latestProjects';
+import DashboardHeader from '@/components/userPage/dashboardHeader';
 
 export default function User() {
   return (
     <div className="flex">
+      <DashboardHeader />
       <Sidebar />
-      <div className="flex-1 flex flex-col">
-        <Header />
-        <main className="p-6 bg-gray-100 flex-1">
-          <Dashboard />
-        </main>
+      <div className="flex-1 p-6 bg-gray-100 pt-[7rem] pl-[15rem]">
+        <Stats />
+        <div className="grid grid-cols-2 gap-4 my-6">
+          <SalesChart />
+          <TrafficSourceChart />
+        </div>
+        <div className="grid grid-cols-2 gap-4">
+          <TopReferrals />
+          <LatestProjects />
+        </div>
       </div>
     </div>
   );
