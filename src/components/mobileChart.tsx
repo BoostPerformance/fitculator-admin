7a1c5812c5062f9b0235763df3294f1f2bf6a037
sung-eunji <danceunji@gmail.com>
2025-05-20 13:22:53 +0200

    feat: add water row at mobile chart

--- src/components/mobileChart.tsx
+++ src/components/mobileChart.tsx
@@ -124,96 +124,88 @@ export default function MobileChart({
 
   //console.log('dietDetailItems', dietDetailItems);
   return (
-    <div className="mt-[3rem] sm:bg-white sm:px-[1rem]">
-      <div className="flex flex-col gap-4">
-        {dietDetailItems.map((meal: ProcessedMeal, index: number) => {
-          // console.log('dietDetailItems', dietDetailItems);
-          return (
-            <div className="pt-[2rem] sm:pt-[0rem]" key={meal.user.id || index}>
-              {/* <button
-                onClick={() =>
-                  router.push(
-                    `/user/${meal.challenge_id}/diet/${meal.user.id}/${selectedDate}`
-                  )
-                }
-                className="text-[#6F6F6F] text-1.125-500 bg-gray-8 p-2 rounded-[0.5rem]"
-              >
-                피드백 남기기 →
-              </button> */}
-              <div
-                onClick={() =>
-                  router.push(
-                    `/user/${meal.challenge_id}/diet/${meal.user.id}/${selectedDate}`
-                  )
-                }
-                className="text-[#6F6F6F] text-1.125-700 pt-[1rem] pl-[1rem] pb-[1rem]"
-              >
-                {meal.user.name}
-              </div>
-
-              <table className="flex flex-col gap-[1rem] items-center justify-center">
-                <thead>
-                  <tr className="flex gap-[2rem] items-center justify-center text-gray-11 text-1-500">
-                    <th>아침</th>
-                    <th>점심</th>
-                    <th>저녁</th>
-                    <th>간식</th>
-                    <th>영양제</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr
-                    key={index}
-                    className="flex gap-[2rem] items-center justify-center"
-                  >
-                    <td>
-                      {isMealUploaded(
-                        meal.daily_records.meals.breakfast[0]?.description || ''
-                      )}
-                    </td>
-                    <td>
-                      {isMealUploaded(
-                        meal.daily_records.meals.lunch[0]?.description || ''
-                      )}
-                    </td>
-                    <td>
-                      {isMealUploaded(
-                        meal.daily_records.meals.dinner[0]?.description || ''
-                      )}
-                    </td>
-                    <td>
-                      {isMealUploaded(
-                        meal.daily_records.meals.snack[0]?.description || ''
-                      )}
-                    </td>
-                    <td>
-                      {isMealUploaded(
-                        meal.daily_records.meals.supplement[0]?.description ||
-                          ''
-                      )}
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-              <div
-                className="w-full text-center pt-[2rem]"
-                onClick={() =>
-                  router.push(
-                    `/user/${meal.challenge_id}/diet/${meal.daily_records.id}/${selectedDate}`
-                  )
-                }
-              >
-                {isfeedback(meal.daily_records.feedback)}
-              </div>
-              <div>
-                {formatDateTime(
-                  meal.daily_records.feedback?.updated_at,
-                  meal.daily_records.feedback?.created_at
-                )}
-              </div>
+    <div className="mt-[1rem] bg-transparent sm:bg-white sm:px-[1rem]">
+      <div className="flex flex-col gap-4 items-center ">
+        {dietDetailItems.map((meal, index) => (
+          <div className="sm:pt-[1rem] w-full" key={meal.user.id || index}>
+            <div
+              onClick={() =>
+                router.push(
+                  `/user/${meal.challenge_id}/diet/${meal.user.id}/${selectedDate}`
+                )
+              }
+              className="text-[#6F6F6F] text-1.125-700 pl-[1rem] pb-[1rem]"
+            >
+              {meal.user.name}
             </div>
-          );
-        })}
+
+            <table className="w-full sm:w-auto flex flex-col items-center justify-center">
+              <thead>
+                <tr className="grid grid-cols-6 gap-[0.4rem] text-gray-11 text-sm sm:text-1.125-500">
+                  <th>아침</th>
+                  <th>점심</th>
+                  <th>저녁</th>
+                  <th>간식</th>
+                  <th>영양제</th>
+                  <th>물</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="mt-6 grid grid-cols-6 gap-[1.6rem]">
+                  <td>
+                    {isMealUploaded(
+                      meal.daily_records.meals.breakfast?.[0]?.description || ''
+                    )}
+                  </td>
+                  <td>
+                    {isMealUploaded(
+                      meal.daily_records.meals.lunch?.[0]?.description || ''
+                    )}
+                  </td>
+                  <td>
+                    {isMealUploaded(
+                      meal.daily_records.meals.dinner?.[0]?.description || ''
+                    )}
+                  </td>
+                  <td>
+                    {isMealUploaded(
+                      meal.daily_records.meals.snack?.[0]?.description || ''
+                    )}
+                  </td>
+                  <td>
+                    {isMealUploaded(
+                      meal.daily_records.meals.supplement?.[0]?.description ||
+                        ''
+                    )}
+                  </td>
+                  <td>
+                    {isMealUploaded(
+                      meal.daily_records.meals.water?.[0]?.description || ''
+                    )}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+
+            <div
+              className="min-w-[16rem] max-w-[22rem] text-center pt-[2rem]"
+              onClick={() =>
+                router.push(
+                  `/user/${meal.challenge_id}/diet/${meal.daily_records.id}/${selectedDate}`
+                )
+              }
+            >
+              {isfeedback(meal.daily_records.feedback)}
+            </div>
+
+            <div>
+              {formatDateTime(
+                meal.daily_records.feedback?.updated_at,
+                meal.daily_records.feedback?.created_at
+              )}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
